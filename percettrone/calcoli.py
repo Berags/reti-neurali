27a29ff32907911b4c32d68b7b163fce833fd35b
Jacopo Beragnoli <bera@BERAPC>
2021-01-26 20:35:38 +0100

    Aggiunti commenti percettrone

--- percettrone/calcoli.py
+++ percettrone/calcoli.py
@@ -16,13 +16,21 @@ def load(file_name):
 
         return array
 
-def calcola_attivazione(w, x):              # Somma pesata
+def calcola_attivazione(w, x):
+    '''
+    Calcola la somma pesata seguendo la formula:
+    s = bias + w * x
+    '''
     res = 0
     for i in range(VROWS + 1):
         res = res + x[i] * w[i]
     return res
 
 def viene_attivato(a):
+    '''
+    Restituisce 1 se il valore della somma pesata è maggiore di 0,
+    altrimenti restituisce 0.
+    '''
     if a > 0:
         return 1
     else:
@@ -35,23 +43,23 @@ x = [1 for i in range(VROWS + 1)]           # Input cognitrone
 out = 0                                     # Valore di output
 w = [0.2, 0.5, -2.5]                        # Pesi cognitrone
 r = 0.05                                    # Velocità apprendimento
-da_stamparex = []
-da_stamparey = []
-markers = []
+da_stamparex = []                           # Array dei valori della X utilizzato per la stampa
+da_stamparey = []                           # Array dei valori della Y utilizzato per la stampa
+markers = []                                # Stile della stampa
 
-# Addestramento
+# Addestriamo la rete neurale
 for i in range(len(data_input)):
-    x = [1, data_input[i][0], data_input[i][1]]
-    a = calcola_attivazione(w, x)
-    attivazione = viene_attivato(a)
+    x = [1, data_input[i][0], data_input[i][1]] # Inizializziamo i valori di x con i dati letti da file
+    a = calcola_attivazione(w, x)               # Calcoliamo la somma pesata
+    attivazione = viene_attivato(a)             # Verifichiamo il valore della somma pesata
     markers.append("*")
     out = data_out[i].item()
     for j in range(VROWS + 1):
-        w[j] = w[j] + r * ((out - attivazione) * x[j])
+        w[j] = w[j] + r * ((out - attivazione) * x[j]) # Utilizziamo la formula w(t + 1) = w(t) + (d - y) * x
     da_stamparex.append(x[1])
     da_stamparey.append(x[2])
 
-# Test
+# Testiamo la rete neurale
 data_test = load('test.txt')
 for i in range(len(data_test)):
     x = [1, data_test[i][0], data_test[i][1]]
--- percettrone/grafico.py
+++ percettrone/grafico.py
@@ -1,18 +1,31 @@
 # importing the required module 
 import matplotlib.pyplot as plt 
 
-plt.ylabel('Asse y')  
+# Nome degli assi
+plt.ylabel('Asse y')
 plt.xlabel('Asse x') 
 
-# Sistemare label il resto funziona
-
 def stampa_grafico(x, y, cls, w, testo):
+    '''
+    Stampa il grafico con i valori calcolati.
+    '''
+    # Titolo della finestra
     plt.suptitle('Modello percettrone - Beragnoli Jacopo', fontsize=15, fontweight='bold')
     plt.title(testo)
-    if w[2] == 0: return
+    # Titolo della finestra
+
+    if w[2] == 0: return # Se w[2] ha valore 0 ritorna --> Non si può dividere per 0!
+
+    # Calcolo dell'equazione della retta
     m = -w[1]/w[2]
     q = -w[0]/w[2]
-    plt.plot([i for i in range(12)], [i*m + q for i in range(12)])
+    # Calcolo dell'equazione della retta
+
+    # Stampiamo la retta mediante l'equazione appena trovata
+    plt.plot([i for i in range(12)], [i*m + q for i in range(12)]) 
     for i in range(len(x)):
+        # Stampa di tutti i punti calcolati dalla rete neurale
         plt.scatter(x[i], y[i], label= "stars", marker= cls[i], color= "black", s=100)  
+    
+    # Mostra la finestra del grafico
     plt.show()
